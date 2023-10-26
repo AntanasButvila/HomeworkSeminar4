26b14c5b1c4e90666c3268a8d37bb3655662b8ba
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-10-26 23:10:32 +0300

    Solved 2 task

--- Program.cs
+++ Program.cs
@@ -1,18 +1,39 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.Write("Введите число А: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число В: ");
-int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"{A}, {B} -> {GetPow(A,B)}");
-int GetPow(int A, int B)
+// Console.Write("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число В: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{A}, {B} -> {GetPow(A,B)}");
+// int GetPow(int A, int B)
+// {
+
+// int result = 1;
+//   for(int i = 0;i < B;i++)
+//   {
+//     result *= A;
+//   }
+//   return result;
+// }
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{number} -> {SumOfDigits(number)}");
+int SumOfDigits(int number)
 {
-  
-int result = 1;
-  for(int i = 0;i < B;i++)
-  {
-    result *= A;
+    int sum = 0;
+  while (number > 0)
+   {
+   sum += number % 10;
+   number/= 10;
   }
-  return result;
+  return sum;
+
 }
